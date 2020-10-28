ceab59431ecfbd62ffc4a4dd26846a340fe130e0
Leonard <medyk170@gmail.com>
2020-10-28 19:45:40 +0100

    add jwt

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
 const port = process.env.PORT || 3000
-
+app.use(express.json())
 
 app.listen(port, () => console.log("Server is running"))
 
 app.post('/login', (req, res) => {
-    const user = [{login: pawel, password: 1234}]
-    res.json(user)
+    const username = req.body.username
+    const user = {name: username}
+    const accessToken = jwt.sign(user, process.env.ACCES_TOKEN_SECRET);
+    res.json({accessToken: accessToken})
 })
 
-app.get('/user', (req, res) => {
-    const user = [{login: 'pawel', password: 1234}]
-    res.json(user)
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    // if we have authHeader return authHeader.split(' ')[1]
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) 
+        return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (err, user) => {
+        if(err)
+            return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+const userinfos = [
+    {
+        username: 'leon',
+        info: "info leona"
+    },
+    {
+        username: 'michal',
+        info: "info michala"
+    }
+]
+
+app.get('/userinfo', authenticateToken , (req, res) => {
+    res.json(userinfos.filter(userinfo => userinfo.username === req.userinfo.username))
 })
 
 app.get('/', (req, res) => {
