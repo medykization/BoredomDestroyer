e3f4881ce5d4a27c7cc42e6bdf634e40c91e22ff
michaaeu <mik3wwl@gmail.com>
2020-10-28 22:25:59 +0100

    Splash Screen with transition added

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/screens/login_screen.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_project/screens/splash_screen.dart';
+import 'dart:async';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-  )); // Transparant Statusbar
-  runApp(MyApp());
-}
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  ); // Transparant Statusbar
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Boredom Destroyer',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SplashScreen(),
+      home: Application(),
+    ),
+  );
+}
+
+class Application extends StatefulWidget {
+  // This widget is the root of your application.
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<Application> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            transitionDuration: Duration(milliseconds: 500),
+            transitionsBuilder: (context, animation, animationTime, child) {
+              return ScaleTransition(
+                alignment: Alignment.center,
+                scale: animation,
+                child: child,
+              );
+            },
+            pageBuilder: (context, animation, animationTime) {
+              return LoginScreen();
+            },
+          ),
+        );
+      },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen();
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -7,27 +7,18 @@ class SplashScreen extends StatelessWidget {
       body: Stack(
         alignment: Alignment.center,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Colors.blue.shade800, Colors.lightBlueAccent.shade100],
-              ),
-            ),
-          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
+              Center(
                 child: new Text(
                   'Boredom',
                   style: TextStyle(
                     fontWeight: FontWeight.normal,
                     fontSize: 40,
                     fontFamily: 'Pacifico',
-                    color: Colors.white70,
+                    color: Colors.blueAccent,
                     shadows: <Shadow>[
                       Shadow(
                         offset: Offset(5.0, 5.0),
@@ -38,14 +29,14 @@ class SplashScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
+              Center(
                 child: new Text(
                   'Destroyer',
                   style: TextStyle(
                     fontWeight: FontWeight.normal,
                     fontSize: 40,
                     fontFamily: 'Pacifico',
-                    color: Colors.white70,
+                    color: Colors.blueAccent,
                     shadows: <Shadow>[
                       Shadow(
                         offset: Offset(5.0, 5.0),
