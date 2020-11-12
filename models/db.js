1df4b2bcfbd647ae6a963f802e7b59b603ab6ea4
michaaeu <mik3wwl@gmail.com>
2020-11-12 23:41:29 +0100

    Fix checkName, checkEmail

--- models/db.js
+++ models/db.js
@@ -12,6 +12,28 @@ async function checkUser(username, password) {
             return results;
         }
         else{
+            client.release();
+            return null;
+        }
+
+    }catch (err) {
+        client.release();
+        return null;
+    }
+};
+
+async function checkEmail(value) {
+    try {
+        const client = await pool.connect();
+        const result = await client.query("SELECT user_email FROM users WHERE user_email = $1", [value]);
+        const results = { 'results': (result) ? result.rows : null};
+
+        if(result.rows[0] != null){
+            client.release();
+            return results;
+        }
+        else{
+            client.release();
             return null;
         }
 
@@ -20,47 +42,48 @@ async function checkUser(username, password) {
     }
 };
 
-async function checkValueInColumn(collumn_name, value) {
+async function checkName(value) {
     try {
         const client = await pool.connect();
-        const selectQuery = 'SELECT *  FROM $1 WHERE $1 = $2 ';
-        const result = await client.query(selectQuery, [collumn_name, value]);
+        const result = await client.query("SELECT user_name FROM users WHERE user_name = $1", [value]);
         const results = { 'results': (result) ? result.rows : null};
 
         if(result.rows[0] != null){
+            client.release();
             return results;
         }
         else{
+            client.release();
             return null;
         }
 
     }catch (err) {
         return null;
-    }finally{
-        client.release();
     }
 };
 
 async function insertUser(username, email, password) {
     try {
         const client = await pool.connect();
-        const selectQuery = 'INSERT INTO users(user_name ,user_email, user_pass) VALUES ($1, $2, $3) ';
+        const selectQuery = 'INSERT INTO users(user_name ,user_email, user_pass) VALUES ($1, $2, $3) RETURNING *';
         const result = await client.query(selectQuery, [username, email, password]);
         const results = { 'results': (result) ? result.rows : null};
 
         if(result.rows[0] != null){
+            client.release();
             return results;
         }
         else{
+            client.release();
             return null;
         }
     }catch (err) {
-        return null;
-    }finally{
         client.release();
+        return null;
     }
 };
 
 exports.checkUser = checkUser
-exports.checkValueInColumn = checkValueInColumn
+exports.checkEmail = checkEmail
 exports.insertUser = insertUser
+exports.checkName = checkName
--- routes/authentication.js
+++ routes/authentication.js
@@ -23,24 +23,37 @@ router.post('/login' ,(req, res) => {
     })
 });
 
-router.post('/registration' ,(req, res) => {
+router.post('/registration',(req, res) => {
     const requser = req.body
 
-    var checkEmail = db.checkUser('user_email', requser.email);
+    var flag = true;
+
+    var checkEmail = db.checkEmail(requser.email);
     checkEmail.then(function(result){
-        if(result.results[0] == null)
-            res.sendStatus(409);
+        if(result != null){
+            console.log("dupaEMAIL");
+            flag = false;
+        }
     })
 
-    var checkName = db.checkUser('user_name', requser.name);
+    
+    var checkName = db.checkName(requser.name);
     checkName.then(function(result){
-        if(result.results[0] == null)
-            res.sendStatus(409);
+        if(result != null){
+            console.log("dupaNAME");
+            flag = false;
+        }
     })
-
+    
+    if(flag == false){
+        res.sendStatus(409);
+        return;
+    }
+   
     var insertUser = db.insertUser(requser.name, requser.email, requser.password)
     insertUser.then(function(result){
-        if(result.results[0] == null)
+        console.log(result.results);
+        if(result == null)
             res.sendStatus(409);
         else{
             const user = {name: requser.name, password: requser.password}
@@ -48,6 +61,7 @@ router.post('/registration' ,(req, res) => {
             res.json({accessToken: accessToken})
         }
     })
+
 });
 
 module.exports = router
