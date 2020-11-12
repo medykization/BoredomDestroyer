460537e4b141d2d6ca25802a8d776d4ff0dd4afb
michaaeu <mik3wwl@gmail.com>
2020-11-12 01:01:22 +0100

    Added getter for db testing

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const authentication = require("./routes/authentication"); 
+const { pool } = require("./dbConfig");
+
+//  middleware
 app.use(express.json());
 
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM users');
+      const results = { 'results': (result) ? result.rows : null};
+      res.json( results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 app.listen(port, () => console.log("Server is running"));
 
