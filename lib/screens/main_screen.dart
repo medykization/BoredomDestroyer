390698ccfaaf055da506478a07bd04b30af65d57
michaaeu <mik3wwl@gmail.com>
2020-11-25 15:15:34 +0100

    Places/Events tab bars added to main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -8,26 +8,41 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        actions: [
-          FlatButton(
-              onPressed: () {
-                // TO DO
-              },
-              child: Icon(Icons.menu),
-              textColor: Colors.white),
-        ],
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: new Center(
-        child: Column(
-          children: <Widget>[
-            // CONTEXT
+    return DefaultTabController(
+      length: 2,
+      child: new Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          actions: [
+            FlatButton(
+                onPressed: () {
+                  // TO DO
+                },
+                child: Icon(Icons.menu),
+                textColor: Colors.white),
           ],
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(text: 'Places'),
+              Tab(text: 'Events'),
+            ],
+          ),
+          backgroundColor: Colors.blueAccent,
         ),
+        body: new Center(
+            child: TabBarView(
+          children: [
+            _buildView('Places'),
+            _buildView('Events'),
+          ],
+        )),
       ),
     );
+  }
+
+  ListView _buildView(String choice) {
+    return ListView.builder(
+        itemBuilder: (context, index) =>
+            ListTile(title: Text(choice + ' $index')));
   }
 }
