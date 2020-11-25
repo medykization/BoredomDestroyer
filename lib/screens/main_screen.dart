2a9a98265832270c13f3ecca9d9eb7135c480b89
michaaeu <mik3wwl@gmail.com>
2020-11-25 13:55:35 +0100

    Main Screen App Bar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'elements/rounded_app_bar.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -11,41 +10,23 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100),
-        child: CustomPaint(
-          size: Size(800, 150),
-          painter: RoundedAppBar(),
-        ),
+      appBar: AppBar(
+        actions: [
+          FlatButton(
+              onPressed: () {
+                // TO DO
+              },
+              child: Icon(Icons.menu),
+              textColor: Colors.white),
+        ],
+        backgroundColor: Colors.blueAccent,
       ),
       body: new Center(
         child: Column(
-          children: <Widget>[_buildWelcomeTextRow()],
+          children: <Widget>[
+            // CONTEXT
+          ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildWelcomeTextRow() {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 40, top: 110),
-      child: Column(
-        children: [
-          Container(
-            child: new Text(
-              'Welcome!',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
-            ),
-          ),
-          Container(
-            child: new Text(
-              'Main Screen',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 20),
-            ),
-          ),
-        ],
       ),
     );
   }
