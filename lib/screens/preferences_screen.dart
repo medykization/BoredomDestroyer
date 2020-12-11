3093690c70c41287cd095db69d82addf26791fc4
Michał <mik3wwl@gmail.com>
2020-12-11 16:43:47 +0100

    Preferences screen modified

--- lib/screens/preferences_screen.dart
+++ lib/screens/preferences_screen.dart
@@ -6,22 +6,30 @@ class PreferencesScreen extends StatefulWidget {
   _PreferencesScreenState createState() => _PreferencesScreenState();
 }
 
-List<String> preferences = [
-  "movie theater",
-  "cafe",
-  "bar",
-  "beauty salon",
-  "bowling alley",
-  "casino",
-  "gym",
-  "zoo",
-  "spa",
-  "shopping mall",
-  "night club",
-  "museum",
-];
+List<String> _categories;
+List<String> _filters;
 
 class _PreferencesScreenState extends State<PreferencesScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _filters = <String>[];
+    _categories = <String>[
+      "movie theater",
+      "cafe",
+      "bar",
+      "beauty salon",
+      "bowling alley",
+      "casino",
+      "gym",
+      "zoo",
+      "spa",
+      "shopping mall",
+      "night club",
+      "museum",
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -35,16 +43,66 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
         backgroundColor: Colors.blueAccent,
         title: Text('Preferences'),
       ),
-      body: Container(
-        child: new GridView.builder(
-          itemCount: preferences.length,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-          itemBuilder: (context, index) {
-            return new Card(
-              child: new GridTile(child: new Text(preferences[index])),
-            );
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Wrap(children: categoryWidgets.toList()),
+              //Text('Selected: ${_filters.join(', ')}'), // TESTING
+              _buildSearchButton()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Iterable<Widget> get categoryWidgets sync* {
+    for (String category in _categories) {
+      yield Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: FilterChip(
+          avatar: CircleAvatar(
+            child: Text(category[0].toUpperCase()),
+          ),
+          label: Text(category),
+          selected: _filters.contains(category),
+          onSelected: (bool selected) {
+            setState(() {
+              if (selected) {
+                _filters.add(category);
+              } else {
+                _filters.removeWhere((String name) {
+                  return name == category;
+                });
+              }
+            });
           },
+        ),
+      );
+    }
+  }
+
+  Widget _buildSearchButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 50, horizontal: 130),
+      child: ButtonTheme(
+        shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(10)),
+        minWidth: 500.0,
+        height: 50.0,
+        child: FlatButton(
+          onPressed: () async {
+            print("Search Button");
+          },
+          child: Text(
+            'Search',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          color: Colors.blueAccent,
         ),
       ),
     );
