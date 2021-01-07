c9b704be9c92182d8787b7c38aefb1d6d4d36188
Michał <mik3wwl@gmail.com>
2021-01-07 21:53:12 +0100

    filename changed: import

--- lib/screens/add_event_screen.dart
+++ lib/screens/add_event_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/models/event.dart';
-import 'package:flutter_project/models/eventCategory.dart';
+import 'package:flutter_project/models/event_category.dart';
 import 'package:flutter_project/screens/main_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
